992b96f6f7bf585a0f710687ab8a39fc1fd72a1a
yyfastow <yoseffastow@gmail.com>
2016-05-19 01:25:58 -0400

    add templates to desplay sluchim and made search bar

--- sliach_rating/urls.py
+++ sliach_rating/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    
+    url(r'^$', views.sluchim_list, name='list'),
+    url(r'search/$', views.search_state, name='search'),
+    url(r'(?P<pk>\d+)/$', views.sliach_details, name='details')
 ]
 
